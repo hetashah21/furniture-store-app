72059804445d751a06251fca32f6bdbf56820d1b
hetashah21 <hetashah2111@gmail.com>
2023-12-10 04:40:59 -0800

    Implemented Login, Registration along with validation and db

---
+++ shop/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///furniture-shop-app.db'
+app.config['SECRET_KEY'] = 'beiubecuewcebocbibeiecpacnok'
+app.app_context().push()
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+from shop.admin import routes
